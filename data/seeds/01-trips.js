82f9a7e91877e632ca6f93f05b097a264076ff05
ari7946 <mercado7946@gmail.com>
2020-04-27 23:27:15 -0700

    Set up migrations and seeds

--- data/seeds/01-trips.js
+++ data/seeds/01-trips.js
@@ -1,9 +1,7 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
   return knex('trips').truncate()
     .then(function () {
-      // Inserts seed entries
       return knex('trips').insert([
         { 
           distance: 4234234.234,
